a78089f44b48a641112e15182ae02b43a32b6806
tobixx0 <Tobi.Stenzel@gmx.de>
2013-03-22 02:35:50 +0100

    init, just a static test

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_extended_project_member_box do
+  name 'Redmine Extended Project Member Box plugin'
+  author 'Tobias Stenzel'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://github.com/PiratenBayernIT/redmine_extended_project_member_box'
+  author_url 'http://github.com/dpausp'
+end
